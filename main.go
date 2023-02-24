90fba6c33866df1521db5ae9be3eb4321778ba42
Pedro Cardozo <p-cardozo@hotmail.com>
2023-02-23 22:25:51 -0300

    Create main.go

---
+++ main.go
@@ -0,0 +1,19 @@
+package goarea
+
+import (
+	"math"
+)
+
+const PI = 3.14156
+
+func Circ(raio float64) float64 {
+	return math.Pow(raio, 2) * PI
+}
+
+func Rect(base, hight float64) float64 {
+	return base * hight
+}
+
+func _TriangleEq(base, hight float64) float64 {
+	return (base * hight) / 2
+}
